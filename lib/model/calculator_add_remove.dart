421e9141541138bffe5b1f0da05d16aa8745e721
Andreas <andreas3nns@googlemail.com>
2019-12-17 14:40:48 +0100

    add class Eintrag

--- lib/model/calculator_add_remove.dart
+++ lib/model/calculator_add_remove.dart
@@ -1,16 +1,27 @@
-class Calculator{
-  double _add;
-  double _remove;
-  double _kontostand;
+class Eintrag{
+  bool _minus;
+  num _betrag;
+  dynamic _category;
+  dynamic _datum;
 
-  Calculator(this._add, this._remove, this._kontostand);
+  Eintrag(bool minus, num betrag, dynamic cat){
+    this._minus = minus;
+    this._betrag = betrag;
+    this._category = cat;
+    this._datum = new DateTime.now();
 
-  void calculateAdd(){
-    _kontostand += _add;
   }
 
-  void calculateRemove(){
-    _kontostand -= _remove;
+  num getBetrag(){
+    if(_minus)return _betrag*-1;
+    else return _betrag;
   }
 
+  dynamic getCat(){
+    return _category;
+  }
+
+  dynamic getDatum(){
+    return _datum;
+  }
 }
