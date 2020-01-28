797a6413c40d78d162c5f02ea34db49c4fb4c2a9
danving <danvi.nguyen31@gmail.com>
2020-01-28 18:16:12 +0100

    Merge: - design_branch to master

--- lib/widgets/bottomNavBar_Widget.dart
+++ lib/widgets/bottomNavBar_Widget.dart
@@ -1,15 +1,9 @@
 import 'package:finanz_app/model/data_model.dart';
-
-import 'package:finanz_app/screens/home_screen.dart';
-import 'package:finanz_app/screens/overview_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
-final pages = <Widget> [
-  HomeScreen(),
-  OverviewScreen(),
-];
+
 
 Widget bottomNavBarWidget(context) {
   return BottomNavigationBar(
